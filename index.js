8878809de10c683b4167e327d20d3d2ead06171b
Sharif Hamza <58281196+Sharif-Hamza@users.noreply.github.com>
2025-03-04 20:56:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import Stripe from 'stripe';import express from 'express';
-import cors from 'cors';
 import Stripe from 'stripe';
 import dotenv from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
